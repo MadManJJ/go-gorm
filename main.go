72f421d3e648a2370c01d3fadacbc0859ba24925
Pannawich Thamart <MadManJJ4949@gmail.com>
2025-05-05 16:16:20 +0700

    Implement simple CRUD operations on Book

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func init() {
@@ -35,11 +38,54 @@ func main() {
   "password=%s dbname=%s sslmode=disable",
   host, port, username, password, databaseName)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// New logger for detailed SQL logging
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Enable color
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 
 	if err != nil {
 		panic("failed to connect database")
 	}
-	fmt.Println(db)
-	fmt.Println("Connect succesfull")
+
+	db.AutoMigrate(&Book{}) // AutoMigrate will not delete col, it can only create col
+	fmt.Println("Migrate succesfull")
+
+	// * Update Book
+	// currentBook := getBook(db, 1) // getBook return an address
+
+	// currentBook.Name = "BOBA JOHN"
+	// currentBook.Price = 440
+
+	// updateBook(db, currentBook)
+	// * --------------------------------
+
+	// * Create Book
+	// createBook(db, &Book{
+	// 	Name: "suzy",
+	// 	Author: "john",
+	// 	Price: 400,
+	// 	Description: "Test",
+	// })
+	// * --------------------------------
+
+	// * Get Book
+	// currentBook := getBook(db,1)
+	// fmt.Println(currentBook)
+	// * --------------------------------
+
+	// * Delete Book
+	deleteBook(db, 1)
+	// * --------------------------------
+
+	// currentBook := getBook(db, 1)
+	// fmt.Println(currentBook)
 }
--- models.go
+++ models.go
@@ -1,9 +1,67 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/gorm"
+)
+
+// * gorm.Model definition
+// type Model struct {
+//   ID        uint           `gorm:"primaryKey"`
+//   CreatedAt time.Time
+//   UpdatedAt time.Time
+//   DeletedAt gorm.DeletedAt `gorm:"index"`
+// }
+
 type Book struct {
-	ID          int64
+	gorm.Model
 	Name        string
 	Author      string
 	Description string
-	price       uint
+	Price       uint
+}
+
+func createBook(db *gorm.DB, book *Book) {
+	result := db.Create(book)
+
+	if result.Error != nil {
+		log.Fatalf("Error creating book: %v", result.Error)
+	}
+
+	fmt.Println("Create Book Successful")
+}
+
+func getBook(db *gorm.DB, id int) *Book {
+	var book Book
+	result := db.First(&book, id) // * first argument is for storing the book we find, second argument is for finding that primary key
+
+	if result.Error != nil {
+		log.Fatalf("Error get book: %v", result.Error)
+	}
+
+	return &book
+}
+
+func updateBook(db *gorm.DB, book *Book) {
+	result := db.Save(book)
+
+	if result.Error != nil {
+		log.Fatalf("Update book failed: %v", result.Error)
+	}
+
+	fmt.Println("Update Book Successful")
+}
+
+func deleteBook(db *gorm.DB, id uint) {
+	var book Book
+	result := db.Delete(&book, id) // ! soft delete if we have DeletedAt gorm.DeletedAt `gorm:"index"`, but hard delete if we don't
+	// result := db.Unscoped().Delete(&book, id) // ! Permanet Delete
+
+	if result.Error != nil {
+		log.Fatalf("Delete book failed: %v", result.Error)
+	}
+
+	fmt.Println("Delete Book Successful")
 }
